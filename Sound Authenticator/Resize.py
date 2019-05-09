fb0c32a77a422dbf19c2c149a341ebbd209e6952
liuchengyuan123 <34617968+liuchengyuan123@users.noreply.github.com>
2019-05-09 21:49:45 +0800

    Create Resize.py

---
+++ Sound Authenticator/Resize.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+
+def resize(name):
+    infile = name
+    outfile = name
+    im = Image.open(infile)
+    (x, y) = im.size
+    x_s = 500
+    y_s = y * x_s // x
+    out = im.resize((x_s, y_s), Image.ANTIALIAS)
+    out.save(outfile)
