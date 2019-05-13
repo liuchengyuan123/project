a39ef9db7a1e039fcd21806af9efc9f037a5ace1
liuchengyuan123 <34617968+liuchengyuan123@users.noreply.github.com>
2019-05-13 22:29:06 +0800

    Update Analyse.py

--- Sound Authenticator/Analyse.py
+++ Sound Authenticator/Analyse.py
@@ -18,8 +18,10 @@ def analyse(arr, start, end, seg, std):
         if i > std:
             cnt += 1
     return cnt / (end - start + 1), toj
-'''
 
+对这段区间的数字按照seg分段，每段里求方差，若方差大于标准值，计数
+返回 计数与所有段数的比值
+'''
 
 def analyse(arr, start, end, seg, std=0.15):
     cnt = 0
@@ -27,7 +29,7 @@ def analyse(arr, start, end, seg, std=0.15):
         if start + (i + 1) * seg >= end:
             break
         xx = arr[start + i * seg: start + (i + 1) * seg]
-        tmp = np.var(xx)
+        tmp = np.var(xx)    #计算方差
         if tmp > std:
             cnt += 1
     return cnt / (end - start + 1)
