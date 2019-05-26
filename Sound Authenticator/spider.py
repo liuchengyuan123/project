45f2def3ff811f5ce886f8979ff86c9e2a79590f
liuchengyuan123 <34617968+liuchengyuan123@users.noreply.github.com>
2019-05-26 22:16:39 +0800

    Update spider.py

--- Sound Authenticator/spider.py
+++ Sound Authenticator/spider.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'}
-html = 'https://ibaotu.com/yinxiao/?chan=bd&label=video&plan=D2-bd&kwd=6705&utm_source=%E7%99%BE%E5%BA%A6SEM&utm_medium=D2-bd&utm_campaign=%E9%9F%B3%E6%95%88-%E9%9F%B3%E6%95%88%E5%85%B6%E4%BB%96&\
-utm_term=%E5%88%9B%E6%84%8F&renqun_youhua=709935&bd_vid=9279238515515122058'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/71.0.3578.80 Safari/537.36'}
+html = 'https://ibaotu.com/yinxiao/?chan=bd&label=video&plan=D2-bd&kwd=6705&\
+            utm_source=%E7%99%BE%E5%BA%A6SEM&utm_medium=D2-bd&utm_campaign=%E9%9F%B3%E6%95%88-%E9%9F%B3%E6%95%88%E5%85%B6%E4%BB%96&\
+            utm_term=%E5%88%9B%E6%84%8F&renqun_youhua=709935&bd_vid=9279238515515122058'
 start = requests.get(html, headers = headers)
 start.encoding = 'utf-8'
 soup = BeautifulSoup(start.text, 'lxml')
