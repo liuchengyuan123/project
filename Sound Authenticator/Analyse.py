27bdca6eed68851222dbe232909599898f469008
liuchengyuan123 <34617968+liuchengyuan123@users.noreply.github.com>
2019-05-26 20:54:15 +0800

    Update Analyse.py

--- Sound Authenticator/Analyse.py
+++ Sound Authenticator/Analyse.py
@@ -2,23 +2,6 @@ import numpy as np
 
 
 '''
-def analyse(arr, start, end, seg, std):
-    toj = []
-    #print(arr)
-    #print(end)
-    for i in range(0, (end - start + 1) // (seg // 2) - 1):
-        #print(i, start + i * seg, start + (i + 1) * seg)
-        if start + (i + 1) * seg >= end:
-            break
-        xx = arr[start + i * seg: start + (i + 1) * seg]
-        tmp = np.var(xx)
-        toj.append(tmp)
-    cnt = 0
-    for i in toj:
-        if i > std:
-            cnt += 1
-    return cnt / (end - start + 1), toj
-
 对这段区间的数字按照seg分段，每段里求方差，若方差大于标准值，计数
 返回 计数与所有段数的比值
 '''
